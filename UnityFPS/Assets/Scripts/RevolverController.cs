e55fee482f17332553818af4a985bc21e4e1fcd6
Vlkyr129 <defcon129@gmail.com>
2020-10-04 01:39:49 -0400

    Reload

--- UnityFPS/Assets/Scripts/RevolverController.cs
+++ UnityFPS/Assets/Scripts/RevolverController.cs
@@ -1,24 +1,60 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class RevolverController : MonoBehaviour
 {
     public Camera fpsCam;
     public GameObject impactEffect;
+    public GameObject barrelEffect;
+    public GameObject ReloadEffect;
 
+    [Header("Gun Characteristics")]
     [SerializeField]
     float damage = 10f;
+    [SerializeField]
+    int maxAmmo = 7;
+    [SerializeField]
+    int currentAmmo;
+    [SerializeField]
+    float releodTime = 5f;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            currentAmmo = maxAmmo;
+        }
+
+        if (Input.GetButtonDown("Fire1") && currentAmmo != 0)
         {
             Shoot();
         }
+
+        if (Input.GetButtonDown("Fire1") && currentAmmo == 0)
+        {
+            NeedReloadPrompt();
+        }
+    }
+
+    void Reload()
+    {
+        currentAmmo = maxAmmo;
     }
 
     public void Shoot()
     {
+        currentAmmo--;
+
+        //Add tweening punch effect to barrel
+        barrelEffect.transform.DORewind();
+        barrelEffect.transform.DOPunchScale(new Vector3(1, 0, .5f), .25f);
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
@@ -28,11 +64,16 @@ public class RevolverController : MonoBehaviour
                 target.TakeDamage(damage);
             }
 
-
-
             //Creates effect on surface hit
             GameObject ImpactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(ImpactGO, .05f);
         }
     }
+
+    void NeedReloadPrompt()
+    {
+        ReloadEffect.transform.DORewind();
+        ReloadEffect.transform.DOPunchScale(new Vector3(.5f, 0, .25f), .25f);
+    }
+     
 }
